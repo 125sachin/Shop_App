c358de4e192d8fef64a0d434be8b2e84060b81b1
sachin <125rathodsachin@gmail.com>
2022-08-11 23:39:15 +0530

    Orders and releated widgets

---
+++ shop_app/lib/screens/orders_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/orders.dart' show Orders;
+import 'package:shop_app/widgets/app_drawer.dart';
+import 'package:shop_app/widgets/order_item.dart';
+
+class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orderScreen';
+  const OrdersScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ordersData = Provider.of<Orders>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Orders'),
+      ),
+      drawer: const AppDrawer(),
+      body: ListView.builder(
+        itemCount: ordersData.orders.length,
+        itemBuilder: (BuildContext context, int index) {
+          return OrderItem(ordersData.orders[index]);
+        },
+      ),
+    );
+  }
+}
---
+++ shop_app/lib/widgets/order_item.dart
@@ -0,0 +1,66 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shop_app/providers/orders.dart' as ord;
+
+class OrderItem extends StatefulWidget {
+  final ord.OrderItem order;
+  const OrderItem(this.order, {Key? key}) : super(key: key);
+
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(10),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text('\$${widget.order.amount}'),
+            subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)),
+            trailing: IconButton(
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+            ),
+          ),
+          if (_expanded)
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: min(widget.order.products.length * 22.0 + 10, 180),
+              child: ListView(
+                children: widget.order.products
+                    .map(
+                      (e) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            e.title,
+                            style: const TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            '${e.quantity}x \$${e.price}',
+                            style: const TextStyle(
+                                fontSize: 18, color: Colors.grey),
+                          )
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            )
+        ],
+      ),
+    );
+  }
+}
