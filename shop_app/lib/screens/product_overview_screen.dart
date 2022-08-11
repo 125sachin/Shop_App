d92dbcbd3e27bbc96c5a4223a7726c504ea47154
sachin <125rathodsachin@gmail.com>
2022-08-11 18:38:57 +0530

    added cart icon button with no.

--- shop_app/lib/screens/product_overview_screen.dart
+++ shop_app/lib/screens/product_overview_screen.dart
@@ -1,16 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
+import 'package:shop_app/screens/cart_screen.dart';
+import 'package:shop_app/widgets/badge.dart';
+// import 'package:provider/provider.dart';
+// import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/widgets/products_grid.dart';
 
-class ProductOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductOverviewScreen extends StatefulWidget {
   const ProductOverviewScreen({Key? key}) : super(key: key);
 
   @override
+  State<ProductOverviewScreen> createState() => _ProductOverviewScreenState();
+}
+
+class _ProductOverviewScreenState extends State<ProductOverviewScreen> {
+  var _showOnlyFavorites = false;
+  @override
   Widget build(BuildContext context) {
+    // final productsContainer = Provider.of<Products>(context,listen: false);
     return Scaffold(
       appBar: AppBar(
         title: const Text("My Shop"),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  // productsContainer.showFavoritesOnly();
+                  _showOnlyFavorites = true;
+                } else {
+                  // productsContainer.showAll();
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+            icon: const Icon(
+              Icons.more_vert,
+            ),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.Favorites,
+                child: Text(
+                  'Only Favorites',
+                ),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.All,
+                child: Text('Show All'),
+              )
+            ],
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              value: cart.itemCount.toString(),
+              child: ch!,
+            ),
+            child: IconButton(
+              icon: const Icon(Icons.shopping_cart),
+              onPressed: () {
+                 Navigator.of(context).pushNamed(CartScreen.roueName);
+              },
+            ),
+          ),
+        ],
       ),
-      body: const ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
