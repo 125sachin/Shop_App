57f29252c830a0fefe90acc4b426cc5030d2524a
sachin <125rathodsachin@gmail.com>
2022-08-11 18:40:43 +0530

    worked on favorite icon

--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -1,41 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
+import 'package:shop_app/providers/product.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
+  const ProductItem({Key? key}) : super(key: key);
 
-  const ProductItem(this.id, this.title, this.imageUrl, {Key? key})
-      : super(key: key);
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+
+  // const ProductItem(this.id, this.title, this.imageUrl, {Key? key})
+  //     : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+
+    final product = Provider.of<Product>(context, listen: false);
+    final cart = Provider.of<Cart>(context,listen: false); 
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            color: Theme.of(context).colorScheme.secondary,
-            onPressed: () {},
-            icon: const Icon(Icons.favorite),
+          leading: Consumer<Product>(
+            builder: (ctx, product, _) => IconButton(
+              color: Theme.of(context).colorScheme.secondary,
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            ),
           ),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
           trailing: IconButton(
-            onPressed: (() {}),
+            onPressed: () {
+              cart.addItem(product.id, product.price, product.title);
+            },
             icon: const Icon(Icons.shopping_cart),
           ),
         ),
         child: GestureDetector(
           onTap: (() {
-            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,arguments: id);
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
           }),
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
