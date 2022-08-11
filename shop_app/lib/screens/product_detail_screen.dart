cdf590f7522571f332b51985a5683e5087a8c754
sachin <125rathodsachin@gmail.com>
2022-08-11 23:41:23 +0530

    product details with img, title, price, descrip

--- shop_app/lib/screens/product_detail_screen.dart
+++ shop_app/lib/screens/product_detail_screen.dart
@@ -5,14 +5,45 @@ import 'package:shop_app/providers/products.dart';
 class ProductDetailScreen extends StatelessWidget {
   // final title;
   const ProductDetailScreen({Key? key}) : super(key: key);
- static const routeName = '/product-detail';
+  static const routeName = '/product-detail';
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context)?.settings.arguments as String;
-    final loadedProduct = Provider.of<Products> (context, listen: false).findById(productId);
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
     return Scaffold(
       appBar: AppBar(
-        title:  Text(loadedProduct.title),
+        title: Text(loadedProduct.title),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                loadedProduct.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              '\$${loadedProduct.price}',
+              style: const TextStyle(
+                  color: Color.fromARGB(255, 99, 99, 99), fontSize: 20),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              loadedProduct.description,
+              textAlign: TextAlign.center,
+              softWrap: true,
+            )
+          ],
+        ),
       ),
     );
   }
