3225c9b3ad20132d4886df51a4a61ec954c11cc5
sachin <125rathodsachin@gmail.com>
2022-08-11 19:08:26 +0530

    used dismissible for swipe and use it's property

--- shop_app/lib/widgets/cart_item.dart
+++ shop_app/lib/widgets/cart_item.dart
@@ -1,31 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
-  const CartItem(this.id,
-  this.price, this.quantity, this.title,
-    {Key? key}) : super(key: key);
+  const CartItem(this.id,this.productId, this.price, this.quantity, this.title, {Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 15,
-      vertical: 4),
-      child: ListTile(
-        leading: CircleAvatar(
-          child: Padding(
-            padding: const EdgeInsets.all(2.0),
-            child: FittedBox(child: Text('\$$price')),
-          ),
-
+    return Dismissible(
+      key: ValueKey(id),
+      // onDismissed: ,
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction){
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
+      },
+      background: Container(
+        color: Theme.of(context).errorColor,
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
         ),
-        title: Text(title),
-        subtitle: Text('Total: \$ ${(price *quantity)}'),
-        trailing: Text('$quantity x'),
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 35,
+        ),
+      ),
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: ListTile(
+          leading: CircleAvatar(
+            child: Padding(
+              padding: const EdgeInsets.all(2.0),
+              child: FittedBox(child: Text('\$$price')),
+            ),
+          ),
+          title: Text(title),
+          subtitle: Text('Total: \$ ${(price * quantity)}'),
+          trailing: Text('$quantity x'),
+        ),
       ),
     );
   }
+
 }
