9931abf2a30cf924960e77a1ed1ec915664d3e16
sachin <125rathodsachin@gmail.com>
2022-08-11 18:37:45 +0530

    crated cart screen using provider

---
+++ shop_app/lib/screens/cart_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart' show Cart ;
+import 'package:shop_app/widgets/cart_item.dart'  ;
+
+class CartScreen extends StatelessWidget {
+  static const roueName = '/cart';
+  const CartScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // final cartList = cart.items.value.toList();
+    final cart = Provider.of<Cart>(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Cart"),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(15),
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Total",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  const Spacer(),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Chip(
+                      label: Text(
+                        '\$${cart.totalAmount}',
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                    ),
+                  ),
+                  OutlinedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      foregroundColor: MaterialStateProperty.all(Colors.purple),
+                    ),
+                    child: const Text("Order Now"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (ctx, i) => CartItem(
+                cart.items.values.toList()[i].id,
+                cart.items.values.toList()[i].price,
+                cart.items.values.toList()[i].quantity,
+                cart.items.values.toList()[i].title ,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
