9c81eca1f6b58af4d2cc7f63b95d5cea11786b32
sachin <125rathodsachin@gmail.com>
2022-08-12 23:44:50 +0530

    added snackbar for added and undo msg

--- shop_app/lib/widgets/product_item.dart
+++ shop_app/lib/widgets/product_item.dart
@@ -16,9 +16,8 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final product = Provider.of<Product>(context, listen: false);
-    final cart = Provider.of<Cart>(context,listen: false); 
+    final cart = Provider.of<Cart>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -31,7 +30,8 @@ class ProductItem extends StatelessWidget {
                 product.toggleFavoriteStatus();
               },
               icon: Icon(
-                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
             ),
           ),
           title: Text(
@@ -40,7 +40,22 @@ class ProductItem extends StatelessWidget {
           ),
           trailing: IconButton(
             onPressed: () {
-              cart.addItem(product.id, product.price, product.title);
+              cart.addItem(
+                product.id,
+                product.price,
+                product.title,
+              );
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: const Text("added"),
+                  duration: const Duration(seconds: 2),
+                  action: SnackBarAction(
+                      label: "UNDO",
+                      onPressed: () {
+                        cart.removeSingleItem(product.id);
+                      }),
+                ),
+              );
             },
             icon: const Icon(Icons.shopping_cart),
           ),
