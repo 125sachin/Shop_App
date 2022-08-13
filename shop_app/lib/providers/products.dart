916a70b901ab8a2c41ccd3d5083a752368e42c0a
sachin <125rathodsachin@gmail.com>
2022-08-13 13:58:16 +0530

    added delete, add, update functions for product

--- shop_app/lib/providers/products.dart
+++ shop_app/lib/providers/products.dart
@@ -60,7 +60,29 @@ class Products with ChangeNotifier {
     return _items.where((element) => element.isFavorite).toList();
   }
 
-  void addProduct() {
+  void updateProduct(String id, Product newProduct) {
+    final prodIndex = _items.indexWhere((element) => element.id == id);
+    if (prodIndex >= 0) {
+      _items[prodIndex] = newProduct;
+      notifyListeners();
+    }else{
+      print('...');
+    }
+  }
+ void deleteProduct(String id){
+  _items.removeWhere((element) => element.id==id);
+  notifyListeners();
+
+ }
+  void addProduct(Product product) {
+    final newProduct = Product(
+      id: product.id,
+      title: product.title,
+      description: product.description,
+      price: product.price,
+      imageUrl: product.imageUrl,
+    );
+    _items.add(newProduct);
     // _items.add(value);
     notifyListeners();
   }
